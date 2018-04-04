90e31faecf9797b4230670e2eb2dede11cfcb98e
Brandon Cormany <brandoncormany@gmail.com>
2018-04-04 16:29:27 -0700

    creating a couple public subnets for VPC

---
+++ public_subnets.tf
@@ -0,0 +1,47 @@
+resource "aws_subnet" "publicA" {
+  vpc_id                  = "${aws_vpc.webapp_vpc.id}"
+  cidr_block              = "10.255.1.0/24"
+  availability_zone       = "${var.region}a"
+  map_public_ip_on_launch = true
+
+  tags {
+    Name = "PubicA"
+  }
+}
+
+resource "aws_subnet" "publicB" {
+  vpc_id                  = "${aws_vpc.webapp_vpc.id}"
+  cidr_block              = "10.255.2.0/24"
+  availability_zone       = "${var.region}b"
+  map_public_ip_on_launch = true
+
+  tags {
+    Name = "PubicB"
+  }
+}
+
+resource "aws_subnet" "publicC" {
+  vpc_id                  = "${aws_vpc.webapp_vpc.id}"
+  cidr_block              = "10.255.3.0/24"
+  availability_zone       = "${var.region}c"
+  map_public_ip_on_launch = true
+
+  tags {
+    Name = "PubicC"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = "${aws_subnet.publicA.id}"
+  route_table_id = "${aws_route_table.public_route.id}"
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = "${aws_subnet.publicB.id}"
+  route_table_id = "${aws_route_table.public_route.id}"
+}
+
+resource "aws_route_table_association" "c" {
+  subnet_id      = "${aws_subnet.publicC.id}"
+  route_table_id = "${aws_route_table.public_route.id}"
+}
