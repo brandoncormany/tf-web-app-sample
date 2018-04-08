dcf882611eda6e580acf11194c8dfaa04cab304b
Brandon Cormany <brandoncormany@gmail.com>
2018-04-07 21:51:32 -0700

    adding call out to EIP in NATGateway

--- nat_gateway.tf
+++ nat_gateway.tf
@@ -1,9 +1,9 @@
-resource "aws_eip" "lb" {
+resource "aws_eip" "natIP" {
   vpc = true
 }
 
 resource "aws_nat_gateway" "natgw" {
-  allocation_id = ""
+  allocation_id = "${aws_eip.natIP.id}"
   subnet_id     = "${aws_subnet.publicA.id}"
 
   tags {
